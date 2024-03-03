05e75250804aa860924d4b0a8c2d17f29a6a9df4
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-03 14:00:54 +0530

    added roman task

---
+++ src/main/java/org/amaap/task/romannumberals/IllegalInputException.java
@@ -0,0 +1,9 @@
+package org.amaap.task.romannumberals;
+
+public class IllegalInputException extends Exception{
+
+    IllegalInputException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/org/amaap/task/romannumberals/Roman.java
@@ -0,0 +1,29 @@
+package org.amaap.task.romannumberals;
+
+public class Roman {
+
+
+    public String convert(int number) throws Exception {
+
+        if (number <= 0) throw new IllegalInputException("Input is not valid");
+
+        StringBuilder sb = new StringBuilder();
+
+        String[] values = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] keys = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        while (number != 0) {
+            for (int i = 0; i < keys.length; i++) {
+
+                if (keys[i] <= number) {
+
+                    number -= keys[i];
+                    sb.append(values[i]);
+                }
+            }
+
+        }
+        return sb.toString();
+
+    }
+}
