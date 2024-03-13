10494751d29d16baa5f135d753e461ee303f6a87
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-13 19:59:22 +0530

    modified creditcardmanagerclass

--- src/main/java/org/amaap/task/creditcard/CreditCardManager.java
+++ src/main/java/org/amaap/task/creditcard/CreditCardManager.java
@@ -3,16 +3,22 @@ package org.amaap.task.creditcard;
 import org.amaap.task.creditcard.domain.CreditCard;
 import org.amaap.task.creditcard.domain.Customer;
 import org.amaap.task.creditcard.domain.exceptions.*;
+import org.amaap.task.creditcard.spendinganalyzer.UnusualSpendAnalyzer;
+import org.amaap.task.creditcard.transactions.Transaction;
+
+import java.util.List;
 
 public class CreditCardManager {
 
 
     private final Customer customer;
     private final CreditCard creditCard;
+    private final UnusualSpendAnalyzer unusualSpendAnalyzer;
 
-    public CreditCardManager(Customer customer, CreditCard creditCard) {
+    public CreditCardManager(Customer customer, CreditCard creditCard, UnusualSpendAnalyzer unusualSpendAnalyzer) {
         this.customer = customer;
         this.creditCard = creditCard;
+        this.unusualSpendAnalyzer = unusualSpendAnalyzer;
     }
 
     public Customer createCustomer(int id, String name, String email) throws InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
@@ -25,5 +31,14 @@ public class CreditCardManager {
 
         return creditCard.createNewCreditCard(cardNumber, newCustomer);
 
+    }
+
+
+    public List<Transaction> getAllTransactions() {
+        return Customer.transactions;
+    }
+
+    public boolean checkForUnusualSpend() {
+        return unusualSpendAnalyzer.isThereIsUnusualSpendingThisMonth();
     }
 }
--- src/test/java/org/amaap/task/creditcard/CreditCardManagerTest.java
+++ src/test/java/org/amaap/task/creditcard/CreditCardManagerTest.java
@@ -3,12 +3,17 @@ package org.amaap.task.creditcard;
 import org.amaap.task.creditcard.domain.CreditCard;
 import org.amaap.task.creditcard.domain.Customer;
 import org.amaap.task.creditcard.domain.exceptions.*;
+import org.amaap.task.creditcard.spendinganalyzer.UnusualSpendAnalyzer;
+import org.amaap.task.creditcard.transactions.Transaction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -21,9 +26,12 @@ public class CreditCardManagerTest {
     Customer customer;
     @Mock
     CreditCard creditCard;
+    @Mock
+    UnusualSpendAnalyzer unusualSpendAnalyzer;
 
     @InjectMocks
     CreditCardManager creditCardManager;
+
 
     @Test
     void shouldAbleToCreateNewCustomer() throws InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
@@ -36,14 +44,36 @@ public class CreditCardManagerTest {
     }
 
     @Test
-    void shouldBeAbleToCreateACreditCardForCustomer() throws InvalidCardNumberException, NullCustomerException {
-//        Customer newCustomer = creditCardManager.createCustomer(1, "Sudhir T", "sudhirtakale99@gmail.com");
+    void shouldBeAbleToCreateACreditCardForCustomer() throws InvalidCardNumberException, NullCustomerException, InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
+        Customer newCustomer = creditCardManager.createCustomer(1, "Sudhir T", "sudhirtakale99@gmail.com");
         when(creditCard.createNewCreditCard(1212, customer)).thenReturn(true);
         boolean result = creditCardManager.createNewCreditCard(1212, customer);
 
         Assertions.assertTrue(result);
     }
 
+
+    @Test
+    void shouldReturnAllTransactions() throws Exception, InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException, InvalidCardNumberException, NullCustomerException {
+        Customer newCustomer = creditCardManager.createCustomer(1, "Sudhir T", "sudhirtakale99@gmail.com");
+        CreditCard card = new CreditCard(1212, newCustomer);
+        Transaction.makeTransaction(1, LocalDate.now(), "Travel", card, 12);
+        Transaction.makeTransaction(2, LocalDate.now(), "Travel", card, 17);
+        Transaction.makeTransaction(3, LocalDate.now(), "Travel", card, 19);
+        List<Transaction> transactions = creditCardManager.getAllTransactions();
+        Assertions.assertEquals(3, transactions.size());
+    }
+
+    @Test
+    void shouldBeAbleToCheckWhetherThereIsAnyUnusualSpendOrNot() throws InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
+        Customer newCustomer = creditCardManager.createCustomer(1, "Sudhir T", "sudhirtakale99@gmail.com");
+        CreditCard card = new CreditCard(1212, newCustomer);
+
+        when(unusualSpendAnalyzer.isThereIsUnusualSpendingThisMonth()).thenReturn(true);
+        boolean result = creditCardManager.checkForUnusualSpend();
+
+        Assertions.assertTrue(result);
+    }
 
 
 
