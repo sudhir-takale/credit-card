90619f3bf23e499e84d20dc3a58afa7059ac9af6
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-25 07:38:50 +0530

    added testcase For sendEmail

--- src/main/java/com/amaap/creditcard/domain/exceptions/InvalidCustomerException.java
+++
@@ -1,7 +0,0 @@
-package com.amaap.creditcard.domain.exceptions;
-
-public class InvalidCustomerException extends Exception {
-    public InvalidCustomerException(String s) {
-        super(s);
-    }
-}
--- src/test/java/com/amaap/creditcard/CreditCardManagerTest.java
+++ src/test/java/com/amaap/creditcard/CreditCardManagerTest.java
@@ -23,7 +23,6 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class CreditCardManagerTest {
 
-
     @Mock
     Customer customer;
     @Mock
@@ -41,61 +40,64 @@ public class CreditCardManagerTest {
 
     @Test
     void shouldAbleToCreateNewCustomer() throws InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
-
+        //Arrange & Act
         Customer user = creditCardManager.createCustomer(1, "Sudhir T", "sudhirtakale99@gmail.com");
+//        Assert
         Assertions.assertNotNull(user);
-
-
     }
 
     @Test
     void shouldBeAbleToCreateACreditCardForCustomer() throws NullCustomerException, InvalidCardNumberException {
+//        Arrange
         when(creditCard.createNewCreditCard(1212, customer)).thenReturn(true);
+//        Act
         boolean result = creditCardManager.createNewCreditCard(1212, customer);
-
+//        Assert
         Assertions.assertTrue(result);
     }
 
-
     @Test
     void shouldBeAbleToCheckWhetherThereIsAnyUnusualSpendOrNot() {
-
+//        Arrange
         when(unusualSpendAnalyzer.isThereIsUnusualSpendingThisMonth()).thenReturn(true);
+//        Act
         boolean result = creditCardManager.checkForUnusualSpend();
-
+//      Assert
         Assertions.assertTrue(result);
     }
 
     @Test
     void shouldAbleToFilterCategoriesWhichHasUnusualSpending() {
+//        Arrange
         when(unusualSpendAnalyzer.isThereIsUnusualSpendingThisMonth()).thenReturn(true);
         when(unusualSpendAnalyzer.categoriesInWhichSpendingIsUnusual()).thenReturn(new HashMap<>());
-
+//        Act
         Map<String, Integer> result = creditCardManager.categoriesInWhichSpendingIsUnusual();
-
+//        Assert
         Assertions.assertEquals(new HashMap<>(), result);
-
         verify(unusualSpendAnalyzer, times(1)).categoriesInWhichSpendingIsUnusual();
     }
 
     @Test
     void shouldAbleToSendEmailMessageToCustomer() throws InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
-
+//        Arrange
         when(unusualSpendAnalyzer.isThereIsUnusualSpendingThisMonth()).thenReturn(true);
         when(creditCardManager.categoriesInWhichSpendingIsUnusual()).thenReturn(new HashMap<>());
-
+//        Act
         Customer customer1 = Customer.createNewCustomer(1, "sudhir t", "Sudhir@gmail.com");
-
+//        assert
         verify(emailHandler, never()).sendEmailToCustomer(new HashMap<>(), customer1);
-
         creditCardManager.sendMessageToCustomer(customer1);
 
     }
 
     @Test
     void shouldBeAbleToSetThresholdPercentageForUnusualSpend() throws InvalidThresholdPercentageException {
+//        Arrange
         when(unusualSpendAnalyzer.setThreshold(149.0)).thenReturn(149.0);
+//        Act
         double result = creditCardManager.setThresholdPercent(149);
+//        Assert
         Assertions.assertEquals(149.0, result);
 
     }
@@ -112,5 +114,30 @@ public class CreditCardManagerTest {
 
     }
 
+    @Test
+    void shouldSendEmailIfCategoriesNotEmpty() throws InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
+        // Arrange
+        Customer customer = Customer.createNewCustomer(1, "sudhir", "sudhirtakale99@gmail.com");
+        Map<String, Integer> categories = new HashMap<>();
+        categories.put("Groceries", 500);
+        when(unusualSpendAnalyzer.isThereIsUnusualSpendingThisMonth()).thenReturn(true);
+        when(creditCardManager.categoriesInWhichSpendingIsUnusual()).thenReturn(categories);
+        // Act
+        creditCardManager.sendMessageToCustomer(customer);
+        // Assert
+        verify(emailHandler).sendEmailToCustomer(categories, customer);
+    }
+
+    @Test
+    void shouldNotSendEmailIfCategoryIsEmpty() throws InvalidCustomerIdException, InvalidCustomerNameException, InvalidCustomerEmailException {
+        // Arrange
+        Customer customer = Customer.createNewCustomer(1, "sudhir", "sudhirtakale99@gmail.com");
+        when(unusualSpendAnalyzer.isThereIsUnusualSpendingThisMonth()).thenReturn(true);
+        when(creditCardManager.categoriesInWhichSpendingIsUnusual()).thenReturn(new HashMap<>());
+        // Act
+        creditCardManager.sendMessageToCustomer(customer);
+        // Assert
+        verify(emailHandler, never()).sendEmailToCustomer(any(), any());
+    }
 
 }
